cbe2615ebe33f01e21fda09dbd98e1c1754d01a4
truc.tran <truc.tran@fullertonhealth.com>
2023-06-15 14:20:38 +0700

    feat(ci): APM-123 abc

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  //   extends: ["@commitlint/config-conventional", "jira"],
-  plugins: ["commitlint-plugin-jira-rules"],
-  extends: ["jira"],
+  //   plugins: ["commitlint-plugin-jira-rules"],
+  //   extends: ["jira", "@commitlint/config-conventional"],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    "jira-task-id-max-length": [2, "always", 10],
-    "jira-task-id-project-key": [2, "always", "APM"],
+    // "jira-task-id-max-length": [2, "always", 10],
+    // "jira-task-id-project-key": [2, "always", "APM"],
+    "subject-case": [2, "never", ["upper-case", "pascal-case", "start-case"]],
   },
 };
