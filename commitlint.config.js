f4b681f4e284ad646176f421509412fd8dfa33ce
truc.tran <truc.tran@fullertonhealth.com>
2023-06-15 14:02:26 +0700

    APM-123: KKK

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,8 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = {
+  //   extends: ["@commitlint/config-conventional", "jira"],
+  plugins: ["commitlint-plugin-jira-rules"],
+  extends: ["jira"],
+  rules: {
+    "jira-task-id-max-length": [2, "always", 10],
+  },
+};
